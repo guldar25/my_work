0875a831ae1c14315193ec0d0137b30f6e7e6e9b
guldar25 <yasybaevag@gmail.com>
2024-11-10 00:24:48 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+completed_work = 12
+hours_spent = 1.5
+course = "Python"
+print(1.5 / 12)
+time_for_one_task = 0.125
+print("курс:",course, ", всего задач:", completed_work, ", затрачено часов:", hours_spent, ", среднее время выполнения", time_for_one_task, "часа")
+
